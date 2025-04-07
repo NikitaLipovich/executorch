d7671e67660157494677cbf5c6b8118b2777a086
NikitaLipovich <nikita1lipovich@gmail.com>
2025-04-07 05:33:02 +0300

    first update and mmd

--- examples/qualcomm/scripts/mmd_nca_net.py
+++ examples/qualcomm/scripts/mmd_nca_net.py
@@ -84,7 +84,7 @@ def main(args):
             data_size=data_num, dataset_dir=args.artifact, download=args.download
         )
 
-    pte_filename = "mmd_nca_net_qualcomm"
+    pte_filename = "mmd_nca_net_qualcomm_8bit"
     instance = MmdNcaNetModel()
 
     build_executorch_binary(
@@ -92,7 +92,7 @@ def main(args):
         instance.get_example_inputs(),
         args.model,
         f"{args.artifact}/{pte_filename}",
-        inputs,
+        None,
         skip_node_id_set=skip_node_id_set,
         skip_node_op_set=skip_node_op_set,
         #quant_dtype=QuantDtype.use_8a8w,
